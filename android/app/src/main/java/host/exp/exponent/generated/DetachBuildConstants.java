2a1354caabb76ba4021d00e7729f99cf125a1d6e
Katarzyna Ziomek-Zdanowicz <ziomecka78@gmail.com>
2018-09-11 17:03:44 +0200

    Android native code update. Working on simulator

--- android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -3,6 +3,6 @@ package host.exp.exponent.generated;
 // This file is auto-generated. Please don't rename!
 public class DetachBuildConstants {
 
-  public static final String DEVELOPMENT_URL = "";
+  public static final String DEVELOPMENT_URL = "expdb74791ce30449c89a1f5ad6c59eb3e0://192.168.56.1:19000";
 
 }
